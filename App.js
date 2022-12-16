e886e880b2cb8f28a0b462b00f1d53d1768fbf5e
Francisco Karlos <karlos251616@gmail.com>
2022-12-16 11:11:13 -0300

    up

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Titulo from './src/components/titulo';
+import Principal from './src/components/principal';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Text style={styles.fonteBlue}>IMC</Text>
+      <Titulo/>
+      <Principal/>
+      <StatusBar styles="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizonte: 15,
+    backgroundColor: '#4682B4',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  fonteBlue: {
+    color: 'BLACK',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+}); 
